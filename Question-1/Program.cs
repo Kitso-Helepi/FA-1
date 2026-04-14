d6a51b85f5a7e3a721d0aa778e02d84436c9c09c
Kitso-Helepi <167878627+Kitso-Helepi@users.noreply.github.com>
2026-04-14 11:08:03 +0200

    Add project files.

---
+++ Question-1/Program.cs
@@ -0,0 +1,25 @@
+﻿class Question1
+{
+    public static void Main()
+    {
+       //Student Personal info
+        Console.Write("Enter student name: ");
+        string studentName = Console.ReadLine();
+        Console.Write("Enter student Surname: ");
+        string studentSurname = Console.ReadLine();
+
+        //Student Marks Input
+        Console.Write("Enter student Mark 1: ");
+        string studentMark1 = Console.ReadLine();
+        int mark1 = int.Parse(studentMark1);
+        string studentMark2 = Console.ReadLine();
+        int mark2 = int.Parse(studentMark2);
+        string studentMark3 = Console.ReadLine();
+        int mark3 = int.Parse(studentMark3);
+
+        //Student Results 
+        Console.WriteLine("Student Name "+ studentName+studentSurname);
+
+
+    }
+}
